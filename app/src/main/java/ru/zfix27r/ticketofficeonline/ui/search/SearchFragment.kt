1014ad8438f84fb9ce8e4a594be45b9972d519a3
Fix <zfix27r@gmail.com>
2024-06-28 23:44:10 +1000

    разработка макета экрана поиска

---
+++ app/src/main/java/ru/zfix27r/ticketofficeonline/ui/search/SearchFragment.kt
@@ -0,0 +1,31 @@
+package ru.zfix27r.ticketofficeonline.ui.search
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.zfix27r.ticketofficeonline.R
+
+class SearchFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SearchFragment()
+    }
+
+    private val viewModel: SearchViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+}
---
+++ app/src/main/java/ru/zfix27r/ticketofficeonline/ui/search/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package ru.zfix27r.ticketofficeonline.ui.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
